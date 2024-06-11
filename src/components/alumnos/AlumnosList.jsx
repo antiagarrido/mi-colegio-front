5221359133e9388fda866ce3b53811a3dc7b5736
ARELANCE\antia.garrido <antiagarrido@gmail.com>
2024-06-11 13:24:37 +0200

    alumnos

--- src/components/alumnos/AlumnosList.jsx
+++ src/components/alumnos/AlumnosList.jsx
@@ -18,11 +18,37 @@ export const AlumnosList = () => {
     return (
         <>
             <h3>Lista de alumnos</h3>
-            <ul>
-                {alumnos.map((alumno) => (
-                    <li key={alumno.id}>{alumno.nombre}</li>
-                ))}
-            </ul>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Teléfono</th>
+                        <th>DNI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {alumnos.map((alumno) => (
+                        <tr key={alumno.id}>
+                            <td>{alumno.id}</td>
+                            <td>{alumno.nombre}</td>
+                            <td>{alumno.apellidos}</td>
+                            <td>{alumno.telefono}</td>
+                            <td>{alumno.dni}</td>
+                            <td>
+                                <button
+                                    class="btn btn-info"
+                                    onClick={() => handleDetailClick(alumno.id)}
+                                >
+                                    Detalles
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 };
