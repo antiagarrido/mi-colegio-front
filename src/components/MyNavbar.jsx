08118a51254227340432d225d6dc29258e60ba38
ARELANCE\antia.garrido <antiagarrido@gmail.com>
2024-06-12 10:04:45 +0200

    mejora navegación

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,52 +1,51 @@
-import React from "react";
+import React from 'react';
 
 const MyNavbar = () => {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        Colegio
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown"
-                        aria-controls="navbarNavDropdown"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse"
-                        id="navbarNavDropdown"
-                    >
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    href="#"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="alumnos">
-                                    Alumnos
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="asignaturas">
-                                    Asignaturas
-                                </a>
-                            </li>
-                            <li className="nav-item dropdown">
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/#">
+            Colegio
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/alumnos">
+                  Alumnos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/asignaturas">
+                  Asignaturas
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/trabajadores">
+                  Trabajadores
+                </a>
+              </li>
+
+              {/* <li className="nav-item dropdown">
                                 <a
                                     className="nav-link dropdown-toggle"
-                                    href="trabajadores"
+                                    href="/trabajadores"
                                     role="button"
                                     data-bs-toggle="dropdown"
                                     aria-expanded="false"
@@ -75,13 +74,13 @@ const MyNavbar = () => {
                                         </a>
                                     </li>
                                 </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+                            </li> */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default MyNavbar;
